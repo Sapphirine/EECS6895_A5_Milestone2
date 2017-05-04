1ee8269c8d63f7e819e7bfa93900415219ed25e6
Aileenlingyu <lz2494@columbia.edu>
2017-05-04 03:55:35 -0400

    Add files via upload

--- object_tracking_lingyu.py
+++ object_tracking_lingyu.py
@@ -1,62 +1,114 @@
 import dlib
 import cv2
 import argparse as ap
-import get_points
 import motionkalm
 
-def run(source=0, dispLoc=False):
-    # Create the VideoCapture object
-    cam = cv2.VideoCapture(source)
+
+
+def Initialization(box)
+
+## Get the initialized bounding box for the selected target
+
+    p_left_list = []
+    p_right_list = []
+
+    box_list = []
+    box_target = box.copy()
+	mousebutton = False
+
+	ix,iy = -1,-1
+
+	cv2.namedWindow("box_target", cv2.WINDOW_NORMAL)
+    cv2.imshow("box_target", box_target)
+# Creating mouse callback function
+	def draw_rect(event, x, y, flags, param):
+		
+		global p_left,p_right,mousebutton
+
+		if event == cv2.EVENT_LBUTTONDOWN:
+			mousebutton = True
+			ix,iy = x,y
+			p_left_list.append((ix,iy))
+
+		elif event == cv2.EVENT_MOUSEMOVE:
+			if mousebutton == True:
+				cv2.rectangle(img,p_left_list[-1],(x,y),(0,255,0),1) 
+
+		elif event == cv2.EVENT_LBUTTONUP:
+			if mousebutton == False:
+				ix,iy = x,y
+				p_right_list.append((ix, iy))
+
+		print "Get target object initialization at [{}, {}]".format(p_left_list[-1], p_right_list[-1])
+
+
+# handle mouse events in OpenCV
+	cv2.setMouseCallback("Inibox", draw_rect)
+
+	key = cv2.waitKey(30)
+	if key == ord('t'):
+		cv2.destroyAllWindows()
+		return zip(p_right_list, p_left_list)
+
+	cv2.destroyAllWindows()
+	return zip(p_right_list, p_left_list)
+
+
+def VideoRead(videopath)
+
+	cam = cv2.VideoCapture(videopath)
 
     # If Camera Device is not opened, exit the program
-    if not cam.isOpened():
-        print "Video device or file couldn't be opened"
-        exit()
+	if not cam.isOpened():
+		print "Video device or file couldn't be opened"
+		exit()
+	
+	print "Press key `p` to pause the video to start tracking"
+	while True:
+        
+		retval, img = cam.read()
+		if not retval:
+			print "Cannot capture frame device"
+			exit()
+		if(cv2.waitKey(10)==ord('p')):
+			break
+		cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+		cv2.imshow("Image", img)
+	cv2.destroyWindow("Image")
+
+
+
+	cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+	cv2.imshow("Image", img)
+
+	obj_box = Initialization(img)
+
+	return obj_box
+
     
-    print "Press key `p` to pause the video to start tracking"
-    while True:
-        # Retrieve an image and Display it.
-        retval, img = cam.read()
-        if not retval:
-            print "Cannot capture frame device"
-            exit()
-        if(cv2.waitKey(10)==ord('p')):
-            break
-        cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
-        cv2.imshow("Image", img)
-    cv2.destroyWindow("Image")
 
-    # Co-ordinates of objects to be tracked 
-    # will be stored in a list named `points`
-    points = get_points.run(img, multi=True) 
+def Tracking(videopath)
 
-    if not points:
-        print "ERROR: No object to be tracked."
-        exit()
 
-    cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
-    cv2.imshow("Image", img)
+	obj = VideoRead(videopath)
 
-    # Initial co-ordinates of the object to be tracked 
-    # Create the tracker object
-    tracker = [dlib.correlation_tracker() for _ in xrange(len(points))]
-    # Provide the tracker the initial position of the object
-    [tracker[i].start_track(img, dlib.rectangle(*rect)) for i, rect in enumerate(points)]
+    tracker = [dlib.correlation_tracker() for _ in xrange(len(obj))]
+   
+    [tracker[i].start_track(img, dlib.rectangle(*rect)) for i, rect in enumerate(obj)]
 
     while True:
-        # Read frame from device or file
+        
         retval, img = cam.read()
         if not retval:
             print "Cannot capture frame device | CODE TERMINATION :( "
             exit()
-        # Update the tracker  
+        
         for i in xrange(len(tracker)):
             tracker[i].update(img)
-            # Get the position of th object, draw a 
-            # bounding box around it and display it.
+            
             rect = tracker[i].get_position()
 
-            # For estimate the occlusion and exiting of the object.
+           
             radio = tracker[i].update(img, rect)
             #print radio
             pt1 = (int(rect.left()), int(rect.top()))
@@ -87,34 +139,43 @@ def run(source=0, dispLoc=False):
 
             pt1 = (int(rect.left()), int(rect.top()))
             pt2 = (int(rect.right()), int(rect.bottom()))
-            cv2.rectangle(img, pt1, pt2, (255, 255, 255), 3)
+            cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
             print "Object {} tracked at [{}, {}] \r".format(i, pt1, pt2),
             if dispLoc:
                 loc = (int(rect.left()), int(rect.top()-20))
 	        txt = "Object tracked at [{}, {}]".format(pt1, pt2)
-	        cv2.putText(img, txt, loc , cv2.FONT_HERSHEY_SIMPLEX, .5, (255,255,255), 1)
+	        cv2.putText(img, txt, loc , cv2.FONT_HERSHEY_SIMPLEX, .5, (0,255,0), 1)
         cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
         cv2.imshow("Image", img)
-        # Continue until the user presses ESC key
+      
         if cv2.waitKey(1) == 27:
             break
 
-    # Relase the VideoCapture object
+    
     cam.release()
 
-if __name__ == "__main__":
-    # Parse command line arguments
-    parser = ap.ArgumentParser()
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-d', "--deviceID", help="Device ID")
-    group.add_argument('-v', "--videoFile", help="Path to Video File")
-    parser.add_argument('-l', "--dispLoc", dest="dispLoc", action="store_true")
-    args = vars(parser.parse_args())
 
-    # Get the source of video
-    if args["videoFile"]:
-        source = args["videoFile"]
-       
-    else:
-        source = int(args["deviceID"])
-    run(source, args["dispLoc"])
+def main(data):
+    Tracking(data)
+
+if __name__ == "__main__":
+	videopath = "/Users/lingyuzhang/Spring17/5AdvancedBigdata/task_milestone/videofile/people.avi" 
+    main(videopath)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
