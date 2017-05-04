8d81a696d9a12f2b73faa4e1d8f3b8a22704e30d
Aileenlingyu <lz2494@columbia.edu>
2017-05-04 04:04:20 -0400

    Add files via upload

--- object_tracking_lingyu.py
+++ object_tracking_lingyu.py
@@ -92,75 +92,75 @@ def Tracking(videopath)
 
 	obj = VideoRead(videopath)
 
-    tracker = [dlib.correlation_tracker() for _ in xrange(len(obj))]
+	tracker = [dlib.correlation_tracker() for _ in xrange(len(obj))]
    
-    [tracker[i].start_track(img, dlib.rectangle(*rect)) for i, rect in enumerate(obj)]
+	[tracker[i].start_track(img, dlib.rectangle(*rect)) for i, rect in enumerate(obj)]
 
-    while True:
+	while True:
         
-        retval, img = cam.read()
-        if not retval:
-            print "Cannot capture frame device | CODE TERMINATION :( "
-            exit()
+		retval, img = cam.read()
+		if not retval:
+			print "Cannot capture frame device | CODE TERMINATION :( "
+			exit()
         
-        for i in xrange(len(tracker)):
-            tracker[i].update(img)
+		for i in xrange(len(tracker)):
+			tracker[i].update(img)
             
-            rect = tracker[i].get_position()
+			rect = tracker[i].get_position()
 
            
-            radio = tracker[i].update(img, rect)
+			radio = tracker[i].update(img, rect)
             #print radio
-            pt1 = (int(rect.left()), int(rect.top()))
-            pt2 = (int(rect.right()), int(rect.bottom()))
+			pt1 = (int(rect.left()), int(rect.top()))
+			pt2 = (int(rect.right()), int(rect.bottom()))
             
-            if radio > best_radio:
-                best_radio = radio
-                print "New highest match!"
+			if radio > best_radio:
+				best_radio = radio
+				print "New highest match!"
             
-            else:
-                if radio < 0.2*best_radio:
+			else:
+				if radio < 0.2*best_radio:
                    #print "Object is exiting or being occluded"
                    # pt1 = (int(rect.left()), int(rect.top()))
                    # pt2 = (int(rect.right()), int(rect.bottom()))
         
                    # kf = kf.em(measurements, n_iter=5)
                    # (filtered_state_means, filtered_state_covariances) = kf.filter(measurements)
-                   kalmanfilter = motionkalm()
+					kalmanfilter = motionkalm()
 
-                   Px, Py = kalmanfilter.ComputeMotion(x,y)
+					Px, Py = kalmanfilter.ComputeMotion(x,y)
 
                    
-                   print "Object is exiting or being occluded, the next prediction position 1s later is:"
-                   print Px
+					print "Object is exiting or being occluded, the next prediction position 1s later is:"
+					print Px
     
-                   break
-                   exit()
+					break
+					exit()
 
-            pt1 = (int(rect.left()), int(rect.top()))
-            pt2 = (int(rect.right()), int(rect.bottom()))
-            cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
-            print "Object {} tracked at [{}, {}] \r".format(i, pt1, pt2),
-            if dispLoc:
-                loc = (int(rect.left()), int(rect.top()-20))
-	        txt = "Object tracked at [{}, {}]".format(pt1, pt2)
-	        cv2.putText(img, txt, loc , cv2.FONT_HERSHEY_SIMPLEX, .5, (0,255,0), 1)
-        cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
-        cv2.imshow("Image", img)
+			pt1 = (int(rect.left()), int(rect.top()))
+			pt2 = (int(rect.right()), int(rect.bottom()))
+			cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
+			print "Object {} tracked at [{}, {}] \r".format(i, pt1, pt2),
+			if dispLoc:
+				loc = (int(rect.left()), int(rect.top()-20))
+			txt = "Object tracked at [{}, {}]".format(pt1, pt2)
+			cv2.putText(img, txt, loc , cv2.FONT_HERSHEY_SIMPLEX, .5, (0,255,0), 1)
+		cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+		cv2.imshow("Image", img)
       
-        if cv2.waitKey(1) == 27:
-            break
+		if cv2.waitKey(1) == 27:
+			break
 
     
-    cam.release()
+	cam.release()
 
 
 def main(data):
-    Tracking(data)
+	Tracking(data)
 
 if __name__ == "__main__":
 	videopath = "/Users/lingyuzhang/Spring17/5AdvancedBigdata/task_milestone/videofile/people.avi" 
-    main(videopath)
+	main(videopath)
 
 
 
